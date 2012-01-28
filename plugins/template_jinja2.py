f53e0c2ae37d83a130d8ce41a1411958173fbfcd
Terr <megakek@gmail.com>
2012-01-28 18:33:55 +0100

    Adds Jinja2 datetime filter

--- plugins/template_jinja2.py
+++ plugins/template_jinja2.py
@@ -1,7 +1,9 @@
 # -*- coding: utf-8 -*-
 import os
-
+import types
+import datetime
 import webber
+
 from webber import set_hook, cfg, relpath, functions
 
 from jinja2 import Environment, FileSystemLoader
@@ -12,6 +14,10 @@ def pagetemplate(params):
     env = Environment(loader=FileSystemLoader(os.path.join(
         os.path.dirname(webber.__file__), cfg.style_dir
     )))
+
+    # Register filters
+    for filter in filters:
+        env.filters[filter.__name__] = filter
 
     template_filename = params.file.template
     if not template_filename.endswith('.html'):
@@ -27,3 +33,22 @@ def pagetemplate(params):
 
     rendered = template.render(**kw)
     return rendered
+
+#
+# Filters
+#
+# Jinja2 filter functions, add to `filters` list to have them automatically
+# added to Jinja2 environment.
+def datetimeformat(value, format='%H:%M / %d-%m-%Y'):
+    if type(value) is types.StringType:
+        value = int(value)
+
+    if type(value) is types.IntType:
+       value = datetime.datetime.fromtimestamp(value)
+    return value.strftime(format)
+
+
+filters = (
+    datetimeformat,
+)
+
